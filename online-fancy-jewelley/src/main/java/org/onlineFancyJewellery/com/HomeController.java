c884b82cf71d80cdcdb3784b86237899c9a4bf25
SriValli <gnsrivalli99@gmail.com>
2017-10-26 13:49:45 +0530

    Succfully loaded static resources

--- online-fancy-jewelley/src/main/java/org/onlineFancyJewellery/com/HomeController.java
+++ online-fancy-jewelley/src/main/java/org/onlineFancyJewellery/com/HomeController.java
@@ -8,8 +8,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -17,23 +20,37 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	/*private static final Logger logger = LoggerFactory.getLogger(HomeController.class);*/
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	
+	@RequestMapping(value = {"/","/home","/index"}, method = RequestMethod.GET)
+	public ModelAndView home() {
+		ModelAndView mv=new ModelAndView("home");
+		mv.addObject("greeting", "Welcome To spring Web MVC");
+		return mv;
 	}
+	
+	
+	/*@RequestMapping(value = {"/test"})
+	public ModelAndView test(@RequestParam(value="greeting",required=false) String greeting) {
+		
+		if(greeting==null){
+			greeting="Hello welcome to spring not hibernate";
+		}
+		ModelAndView mv=new ModelAndView("home");
+		mv.addObject("greeting",greeting);
+		return mv;
+	}
+	
+	@RequestMapping(value = {"/test/{test}"})
+	public ModelAndView test(@PathVariable(value="test") String greeting) {
+		
+		if(greeting==null){
+			greeting="Hello welcome to spring not hibernate";
+		}
+		ModelAndView mv=new ModelAndView("home");
+		mv.addObject("greeting",greeting);
+		return mv;
+	}*/
 	
 }
